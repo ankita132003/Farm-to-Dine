f9257d1c2180bbd78238ff9d77defc34c7bf5d87
Ankita Yadav <ankitayadav80048@gmail.com>
2023-03-21 00:10:10 +0530

    newchanges

--- src/components/LandOwner.vue
+++ src/components/LandOwner.vue
@@ -1,45 +1,58 @@
 <template>
-    <div class="m-2">
-        <h3> Advance Features </h3>
-        <button class="btn btn-primary" @click="ContactFarming">
-            Contact Farming 
-        </button>
-    </div>
-    <div>
-        <h4>
-           Your Active Contracts 
-        </h4>
-        <div class="row">
-        <div class="col-md-4" v-for ='(items ,  index ) in contract' :key="index">
-            <div class="card m-3 p-2" style="width: 20rem, height:20rem" >
-               <p>Name : {{items.name}}</p>
-               <p>Area : {{items.area}}</p>
-                <p>Min Price : {{items.price}}</p>
-                <button class="btn btn-primary" @click="ContactFarming">
-                    View Bids & Clause</button>
-            </div>
+  <div class="m-2">
+    <h3>Advance Features</h3>
+    <hr />
+    <button class="btn btn-primary" @click="ContactFarming">
+      Contact Farming
+    </button>
+  </div>
+  <div>
+    <h3>Your Active Contracts</h3>
+    <hr>
+    <div class="row">
+      <div class="col-md-4 " v-for="(items, index) in contract" :key="index">
+        <div class="card m-3 p-2 contractors" style="width: 20rem, height:20rem">
+          <p>Name : {{ items.name }}</p>
+          <p>Area : {{ items.area }}</p>
+          <p>Min Price : {{ items.price }}</p>
+          <button class="btn btn-primary" @click="ContactFarming">
+            View Bids & Clause
+          </button>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "LandOwner",
-    props: {
-        contract: Object,
+  name: "LandOwner",
+  props: {
+    contract: Object,
+  },
+  data() {
+    return {
+      crop: [],
+      listing: [],
+      user: {},
+    };
+  },
+  methods: {
+    ContactFarming() {
+      this.$router.push({ name: "ContactFarming" });
     },
-    data() {
-        return {
-            crop: [],
-            listing: [],
-            user: {},
-        };
-    },
-    methods:{
-        ContactFarming(){
-            this.$router.push({name: 'ContactFarming'});
-        }
-    },
+  },
+};
+</script>
+<style>
+    .btn{
+        margin-bottom: 30px;
+        width: 200px;
+        height: 43px;
+        font-size: 18px;
+        
     }
-    </script>
+    .contractors{
+        background-color: #DDFFD9;
+    }
+</style>
